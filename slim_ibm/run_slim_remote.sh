ef9bc4c69fece9390d01ee79a7d781105bf55cd7
Andrea Chen <chenz20@noether.csse.rose-hulman.edu>
2024-01-22 09:38:28 -0500

    update from server

--- slim_ibm/run_slim_remote.sh
+++ slim_ibm/run_slim_remote.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 declare c=0
 declare max=40
-declare date='Jan28'
+declare date='Jan29'
 # current folder is slim_ibm
 mkdir "../../thesis_result"
 mkdir "../../thesis_result/data_set"
 mkdir "../../thesis_result/data_set/${date}"
 
 
-# jump-signal
-for i in {1..50}
-do
-   let "c+=1"
-   slim -d "filename='../../thesis_result/data_set/${date}/jump_signal_${i}'" -d "jump_enabled=1" model.slim &
-   if ((c>$max))
-   then
-      wait
-      let "c=0"
-   fi
-done
+# # jump-signal
+# for i in {1..50}
+# do
+#    let "c+=1"
+#    slim -d "filename='../../thesis_result/data_set/${date}/jump_signal_${i}'" -d "jump_enabled=1" model.slim &
+#    if ((c>$max))
+#    then
+#       wait
+#       let "c=0"
+#    fi
+# done
 
-# jump-choosiness
-for i in {1..50}
-do
-   let "c+=1"
-   slim -d "filename='../../thesis_result/data_set/${date}/jump_choosiness_${i}'" -d "jump_enabled=1" -d "jump_trait='choosiness'" model.slim &
-   if ((c>$max))
-   then
-      wait
-      let "c=0"
-   fi
-done
+# # jump-choosiness
+# for i in {1..50}
+# do
+#    let "c+=1"
+#    slim -d "filename='../../thesis_result/data_set/${date}/jump_choosiness_${i}'" -d "jump_enabled=1" -d "jump_trait='choosiness'" model.slim &
+#    if ((c>$max))
+#    then
+#       wait
+#       let "c=0"
+#    fi
+# done
 
 # stop mutation-signal
 for i in {1..50}
